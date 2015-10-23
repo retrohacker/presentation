8840d10e467160670a04bbe19af62a06e0dbadee
wblankenship <william.jblankenship@gmail.com>
2015-10-23 11:45:35 -0400

    Version to present

---
+++ 2/producer/producer/app.js
@@ -0,0 +1,17 @@
+var express = require('express')
+var app = express()
+
+var response = "Hello World!"
+
+
+app.get('/', function (req, res) {
+  res.send(response)
+})
+
+var server = app.listen(3000, function () {
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log('Example app listening at http://%s:%s', host, port)
+})
+
